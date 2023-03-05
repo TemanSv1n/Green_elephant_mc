994669bb6613c825b891b9a71b2cb0785ab21aea
TemanSv1n <temasyr.o@gmail.com>
2023-03-05 14:59:59 +0300

    MCreator Merge commit '291904a11d310289a66960a65f5cbe559fcad595' into e5acb1705f0cd2e01d911542ff59990d4c0777f6

--- src/main/java/net/svisvi/slonik/init/SlonikModBlocks.java
+++ src/main/java/net/svisvi/slonik/init/SlonikModBlocks.java
@@ -4,7 +4,6 @@
  */
 package net.svisvi.slonik.init;
 
-import net.svisvi.slonik.block.PskBlock;
 import net.svisvi.slonik.block.EmptyplateBlock;
 import net.svisvi.slonik.block.BreadplateBlock;
 import net.svisvi.slonik.SlonikMod;
@@ -23,7 +22,6 @@ public class SlonikModBlocks {
 	public static final DeferredRegister<Block> REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, SlonikMod.MODID);
 	public static final RegistryObject<Block> BREADPLATE = REGISTRY.register("breadplate", () -> new BreadplateBlock());
 	public static final RegistryObject<Block> EMPTY_PLATE = REGISTRY.register("empty_plate", () -> new EmptyplateBlock());
-	public static final RegistryObject<Block> PSK = REGISTRY.register("psk", () -> new PskBlock());
 
 	@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 	public static class ClientSideHandler {
